7f9816e898c552e1ad3d3bde93d4cbeee1a7758d
mariolsuarez <marioleosuarez@gmail.com>
2020-02-26 13:20:34 -0500

    Feature: added refernce comment

--- Statistics/StandardDeviation.py
+++ Statistics/StandardDeviation.py
@@ -1,5 +1,7 @@
 import statistics
 
+#reference: https://docs.python.org/3/library/statistics.html
+
 
 def standard_deviance(data):
     solution = statistics.stdev(data)
--- Statistics/Variance.py
+++ Statistics/Variance.py
@@ -1,5 +1,7 @@
 import statistics
 
+#reference: https://docs.python.org/3/library/statistics.html
+
 def variance (data):
     solution = statistics.variance(data, xbar=None)
 
