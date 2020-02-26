97f8d26d83a419e115ce50ae09f023c104b4b872
mariolsuarez <marioleosuarez@gmail.com>
2020-02-26 13:29:59 -0500

    Fix: added standard deviation and variance methods to statisctis class

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -2,6 +2,8 @@ from Calculator.Calculator import Calculator
 from Statistics.Mean import mean
 from Statistics.Mode import mode
 from Statistics.Median import median
+from Statistics.StandardDeviation import standard_deviance
+from Statistics.Variance import variance
 
 
 class Statistics(Calculator):
@@ -18,4 +20,12 @@ class Statistics(Calculator):
 
     def median (self, data):
         self.result = median(data)
+        return self.result
+
+    def standard_deviance (self, data):
+        self. result = standard_deviance(self, data)
+        return self.result
+
+    def variance (self, data):
+        self.result = variance(self, data)
         return self.result
