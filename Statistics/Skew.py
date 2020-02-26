f13cefd9ef76596af65e1129e079e27a0042fab0
mariolsuarez <marioleosuarez@gmail.com>
2020-02-26 14:16:07 -0500

    Task: create skew method

---
+++ Statistics/Skew.py
@@ -0,0 +1,18 @@
+
+from Statistics.Mean import mean
+from Statistics.Variance import variance
+from Statistics.StandardDeviation import standard_deviance
+
+#reference https://realpython.com/python-statistics/
+'''
+def skewness (data):
+
+    dataLen = len(data)
+
+    mn = mean(data)
+    var = variance(data)
+    std = standard_deviance(data)
+
+    skew = 
+    
+'''
