986e3a70d1254ca8875640a4f7b56783ce53dc33
mariolsuarez <marioleosuarez@gmail.com>
2020-02-25 19:50:34 -0500

    Task: edited out random num gen list with seed

--- Radom/Random.py
+++ Radom/Random.py
@@ -1,6 +1,10 @@
 #Random class
 #random methods
-import random
+
+from random import random
+from random import seed
+import numpy as np
+import pprint
 
 class Random:
 
@@ -21,3 +25,24 @@ class Random:
         # Generate a random number with a seed between a range of two numbers - Decimal
         random.seed (5)
         random_flt =  round(random.uniform(1,11), 2)
+
+        return (random_flt, random_int)
+'''
+    def random_list_seed(numRange, lstLen):
+
+        #np.arrange(listLen).reshape(1,1, listLen)
+        # Generates a list of N random numbers with a seed and between a range of numbers - Both Integer and Decimal
+
+        random_ints = random.sample(range(numRange), lstLen)
+
+        # Generates a list of N random numbers with a seed and between a range of numbers - Both Integer and Decimal
+        random.seed(2)
+        random_flts = []
+        for i in range(lstLen):
+            value = random()
+            value = round(value, 2)
+            random_flts.append(value)
+
+        return (random_ints, random_flts)
+
+'''
