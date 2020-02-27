95381f678be33bc585ddece7c449638799af80cd
mariolsuarez <marioleosuarez@gmail.com>
2020-02-27 12:17:23 -0500

    Feature: created class for what type of items to return including: random number, seeded number, random list, and seeded list

---
+++ RandomNumGen/ListType.py
@@ -0,0 +1,35 @@
+import random
+from RandomNumGen.RandomNumber import RandomNumber
+from RandomNumGen.RandomList import RandomList
+
+class ItemReturnType:
+
+    @staticmethod
+    def random_num(data):
+        #returns random number from a list (NO seed)
+        num = random.choice(data)
+        return num
+
+    @staticmethod
+    def random_num_seed(data, seed):
+        # returns random number from a list (WITH seed)
+        random.seed(seed)
+        num = ItemReturnType.num(data)
+        return num
+
+    @staticmethod
+    def aList(data, lstLen):
+        lst = []
+        while True:
+            num = ItemReturnType.random_num(data)
+            lst.append(num)
+            if len(lst) < lstLen:
+                continue
+
+        return lst
+
+    @staticmethod
+    def aList_seed(data, lstLen, seed):
+        random.seed(seed)
+        lst = ItemReturnType.aList(data, lstLen)
+        return lst
