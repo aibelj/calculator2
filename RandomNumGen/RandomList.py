084f413d03047d3705468dc58856b3ee3a1af08e
mariolsuarez <marioleosuarez@gmail.com>
2020-02-27 11:31:06 -0500

    Feature: created method that creates list of random numbers using random num generator

--- RandomNumGen/RandomList.py
+++ RandomNumGen/RandomList.py
@@ -1,1 +1,17 @@
-from random import random
+import random
+
+class RandomList:
+
+    @staticmethod
+    def random_list(low, high, length, seed):
+        #creates a list of random numbers by callin random_number_seed method
+        data = []
+
+        while True:
+            number = RandomNumber.random_number_seed(low, high, seed)
+            data.append(number)
+
+            if len(data) == length:
+                break
+
+        return data
