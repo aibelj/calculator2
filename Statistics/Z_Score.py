9806de87c5093c030ce041c6f5030657cab02d7b
mariolsuarez <marioleosuarez@gmail.com>
2020-02-29 18:40:10 -0500

    Fix: zscore

--- Statistics/Z_Score.py
+++ Statistics/Z_Score.py
@@ -1,18 +1,12 @@
 from Statistics.Mean import Mean
 from Statistics.StandardDeviation import StandardDeviation
 from Calculator.Division import division
+from Calculator.Calculator import subtraction
 
-class ZScore():
 
+class Z_Score:
     @staticmethod
-    def z_score(a, b, c):
-
-        a = int(a)
-        b = int(b)
-        c = int(c)
-        value = [a,b,c]
-        std_dev = StandardDeviation.standard_deviance(a, b, c)
-        mn = Mean.mean(a, b, c, 3)
-        result = division((a - mn ), std_dev)
-
-        return result
+    def zscore(data):
+        z = subtraction(data, Mean.mean(data))
+        zscore = division(z, StandardDeviation.standard_deviance(data))
+        return zscore
