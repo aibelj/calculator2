2b9f21a5fc302ccf95989553fb6b7feb3828d7ee
mariolsuarez <marioleosuarez@gmail.com>
2020-03-02 12:43:46 -0500

    Fix: zscore

--- Statistics/Z_Score.py
+++ Statistics/Z_Score.py
@@ -1,12 +1,12 @@
-from Statistics.Mean import Mean
 from Statistics.StandardDeviation import StandardDeviation
-from Calculator.Division import division
-
-
+from Statistics.Mean import Mean
+from RandomNumGen.ItemReturnType import ItemReturnType
 
 class Z_Score:
     @staticmethod
-    def zscore(data):
-        z = subtraction(data, Mean.mean(data))
-        zscore = division(z, StandardDeviation.standard_deviance(data))
-        return zscore
+    def zscore(data, seed):
+        n = ItemReturnType.random_num_seed(data, seed)
+        mn = Mean.mean(data)
+        sd = StandardDeviation.standard_deviance(data)
+        zs = (n - mn) / sd
+        return zs
