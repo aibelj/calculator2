Fix: standard deviation

Statistics/StandardDeviation.py
@@ -1,9 +1,9 @@
-import statistics
+import numpy as np
 
 #reference: https://docs.python.org/3/library/statistics.html
 
 class StandardDeviation():
     @staticmethod
     def standard_deviance(data):
-        solution = statistics.stdev(data)
-        return solution
+        sd = np.std(data)
+        return sd